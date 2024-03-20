70d5521b6111cdce685f27605c7c7cb3c8a3cfa1
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-19 22:01:51 -0500

    test: create a MovieResult with attributes

---
+++ spec/poros/movie_result_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe MovieResult do
+  it "is a movie with attributes" do
+    attributes = {
+      title: "Howl's Moving Castle"
+      vote_average: "9"
+      movie_id: "55"
+    }
+
+    movie_result = MovieResult.new(attributes)
+
+    expect(movie_result).to be_a MovieResult
+    expect(movie_result.title).to eq("Howl's Moving Castle")
+    expect(movie_result.vote_average).to eq("9")
+    expect(movie_result.tmdb_id).to eq("55")
+  end
+end
