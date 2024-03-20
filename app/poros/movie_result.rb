499069c962ce776b5d27750818767eb8e4cd0750
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-19 22:03:24 -0500

    MovieResult with some fabulous attributes

---
+++ app/poros/movie_result.rb
@@ -0,0 +1,11 @@
+class Movie
+  attr_reader :title,
+              :vote_average,
+              :movie_id
+
+  def initialize(attributes)
+    @title = attributes[:title]
+    @vote_average = attributes[:vote_average]
+    @movie_id = attributes[:id]
+  end
+end
