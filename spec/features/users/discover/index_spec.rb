f3a484e502162ef90f17073cc2f0690a0c4b4528
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-19 19:37:28 -0500

    test: list top rated and title search

--- spec/features/users/discover/index_spec.rb
+++ spec/features/users/discover/index_spec.rb
@@ -21,5 +21,27 @@ RSpec.describe "User's Discover Page", type: :feature do
         expect(page).to have_button("Search by Movie Title")
       end
     end
+
+    descibe "Discover Top Rate Movies button" do
+      it "redirects to Movies Index which displays up to 20 top rated movies" do
+        click_button "Discover Top Rated Movies"
+        expect(current_path).to eq(user_movies_path(user_1))
+        expect(page).to have_content("Vote Average:", count: 20)
+      end
+    end
+
+    describe "Search by Movie Title" do
+      it "redirects to Movies Index which displays up to 20 movies w/the matching title keyword(s)" do
+        fill_in :keyword, with: "All Dogs"
+        click_button "Search by Movie Title"
+        expect(current_path).to eq(user_movies_path(user_1))
+        # expect(page).to have_content()
+      end
+
+      it "can't redirect if no keyword entered" do
+        click_button "Search by Movie Title"
+        expect(current_path).to eq(user_discover_index_path(user_1))
+      end
+    end
   end
 end
