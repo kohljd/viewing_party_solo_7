f7eddb84ba98f7ba0b62923202366e6c9b03046d
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-21 16:09:43 -0500

    refactor: simplify facade and service methods

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,19 +1,23 @@
 class MovieFacade
   def self.movie(movie_id)
-    service = MovieService.new
-    json = service.movie_details(movie_id)
-    @movie = Movie.new(json)
+    json = MovieService.movie_details(movie_id)
+
+    Movie.new(json)
   end
 
   def self.cast_members(movie_id)
-    service = MovieService.new
-    json = service.cast_members(movie_id)
-    @cast_members = json[:cast].take(10).map {|cast_member_info| CastMember.new(cast_member_info)}
+    json = MovieService.cast_members(movie_id)
+
+    json[:cast].take(10).map do |cast_member_info| 
+      CastMember.new(cast_member_info)
+    end
   end
 
   def self.reviews(movie_id)
-    service = MovieService.new
-    json = service.reviews(movie_id)
-    @reviews = json[:results].map {|review_data| MovieReview.new(review_data)}
+    json = MovieService.reviews(movie_id)
+
+    json[:results].map do |review_data| 
+      MovieReview.new(review_data)
+    end
   end
 end
--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,22 +1,22 @@
 class MovieService
-  def movie_details(movie_id)
+  def self.movie_details(movie_id)
     get_url("/3/movie/#{movie_id}")
   end
 
-  def cast_members(movie_id)
+  def self.cast_members(movie_id)
     get_url("/3/movie/#{movie_id}/credits")
   end
 
-  def reviews(movie_id)
+  def self.reviews(movie_id)
     get_url("/3/movie/#{movie_id}/reviews")
   end
 
-  def get_url(url)
+  def self.get_url(url)
     response = conn.get(url)
     data = JSON.parse(response.body, symbolize_names: true)
   end
   
-  def conn
+  def self.conn
     Faraday.new(url: "https://api.themoviedb.org") do |faraday|
       faraday.params["api_key"] = Rails.application.credentials.tmdb[:api_key]
     end
