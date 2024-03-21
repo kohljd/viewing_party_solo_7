b7479eeee7aa12a35cc37ad7a9fce71c8d89a19f
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-03-20 22:53:51 -0500

    format date movie review created

--- app/poros/movie_review.rb
+++ app/poros/movie_review.rb
@@ -1,11 +1,19 @@
 class MovieReview
   attr_reader :review_id,
               :author,
-              :content
+              :content,
+              :movie_rating,
+              :created_at
 
   def initialize(attributes)
     @review_id = attributes[:id]
     @author = attributes[:author]
     @content = attributes[:content]
+    @movie_rating = attributes[:author_details][:rating]
+    @created_at = attributes[:created_at]
+  end
+
+  def date_written
+    Date.parse(@created_at).to_fs(:long_ordinal)
   end
 end
--- spec/poros/movie_review_spec.rb
+++ spec/poros/movie_review_spec.rb
@@ -3,16 +3,38 @@ require "rails_helper"
 RSpec.describe MovieReview do
   it "is a movie review with attributes" do
     attributes = {
-      id: 561958,
+      id: "5ddbc870aad9c20012c232bd",
       author: "Martin",
-      content: "Such a great review"
+      content: "Such a great review",
+      author_details: {
+        rating: 9.0
+      },
+      created_at: "2019-11-25T12:26:24.819Z"
     }
 
     movie_review = MovieReview.new(attributes)
     
     expect(movie_review).to be_a MovieReview
-    expect(movie_review.review_id).to eq(561958)
+    expect(movie_review.review_id).to eq("5ddbc870aad9c20012c232bd")
     expect(movie_review.author).to eq("Martin")
     expect(movie_review.content).to eq("Such a great review")
+    expect(movie_review.movie_rating).to eq(9.0)
+    expect(movie_review.created_at).to eq("2019-11-25T12:26:24.819Z")
+  end
+
+  it "formats date review created" do
+    attributes = {
+      id: "5ddbc870aad9c20012c232bd",
+      author: "Martin",
+      content: "Such a great review",
+      author_details: {
+        rating: 9.0
+      },
+      created_at: "2019-11-25T12:26:24.819Z"
+    }
+
+    movie_review = MovieReview.new(attributes)
+
+    expect(movie_review.date_written).to eq("November 25th, 2019")
   end
 end
