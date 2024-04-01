1816c4eee54a2cd1e78d2815804a9b618f5737ac
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-01 15:09:37 -0500

    setup user passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-   validates_presence_of :name, :email
+   validates_presence_of :name, :email, :password
    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+   has_secure_password
 
    has_many :user_parties
    has_many :viewing_parties, through: :user_parties
