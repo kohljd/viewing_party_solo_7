3f613de87b8e37f3bd850db089f846e8e783698b
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-04-01 15:59:42 -0500

    update create new user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-   validates_presence_of :name, :email, :password
+   validates_presence_of :name, :email
    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+   validates :password, presence: true, confirmation: true
 
    has_secure_password
 
